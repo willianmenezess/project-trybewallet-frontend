e15cf679347859e7d4d59a9b0d3229d523a85c8e
willianmenezess <willmenezes@outlook.com.br>
2023-04-13 12:04:54 -0300

    feat: resolve requisito 7 - cria tabela que terá as despesas adicionadas

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,7 +3,25 @@ import React, { Component } from 'react';
 class Table extends Component {
   render() {
     return (
-      <div>Table</div>
+      <section>
+        <table>
+          <tr className="flex gap-7">
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+          <tr className="flex gap-7">
+            <td>segunda linha</td>
+          </tr>
+        </table>
+
+      </section>
     );
   }
 }
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Header from '../components/Header';
+import Table from '../components/Table';
 import WalletForm from '../components/WalletForm';
 import { fetchCurrencyThunk } from '../redux/actions';
 
@@ -16,6 +17,7 @@ class Wallet extends React.Component {
       <header>
         <Header />
         <WalletForm />
+        <Table />
       </header>
     );
   }
