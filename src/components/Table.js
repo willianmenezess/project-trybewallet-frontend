11bd5579871ad540a6d20abb63a951657eb92196
willianmenezess <willmenezes@outlook.com.br>
2023-04-13 16:59:09 -0300

    feat: resolve requisito 7 - tabela de despesas alimentada com os valores do estado global da aplicação

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Table extends Component {
   render() {
+    const { expenses } = this.props;
     return (
       <section>
         <table>
-          <tr className="flex gap-7">
+          {/* <colgroup span={ expenses.length } className="columns" /> */}
+          <tr>
             <th>Descrição</th>
             <th>Tag</th>
             <th>Método de pagamento</th>
@@ -16,14 +20,47 @@ class Table extends Component {
             <th>Moeda de conversão</th>
             <th>Editar/Excluir</th>
           </tr>
-          <tr className="flex gap-7">
-            <td>segunda linha</td>
-          </tr>
-        </table>
+          <tbody>
+            { expenses.map((expense) => (
+              <tr key={ expense.id }>
+                <td>{expense.description}</td>
+                <td>{expense.tag}</td>
+                <td>{expense.method}</td>
+                <td>{Number(expense.value).toFixed(2)}</td>
+                <td>
+                  {expense.exchangeRates[expense.currency].name}
 
+                </td>
+                <td>{Number(expense.exchangeRates[expense.currency].ask).toFixed(2)}</td>
+                <td>
+                  {(Number(expense.value)
+                  * Number(expense.exchangeRates[expense.currency].ask)).toFixed(2)}
+                </td>
+                <td>Real</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </section>
     );
   }
 }
 
-export default Table;
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+Table.propTypes = {
+  expenses: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      value: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+      method: PropTypes.string.isRequired,
+      tag: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+};
+
+export default connect(mapStateToProps)(Table);
