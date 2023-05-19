95f329bb988f19708844c173a46e30b7b1618cb6
willianmenezess <willmenezes@outlook.com.br>
2023-05-19 19:20:42 -0300

    fix: bug

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 import { Provider } from 'react-redux';
 import App from './App';
@@ -11,9 +11,9 @@ ReactDOM
   .createRoot(document.getElementById('root'))
   .render(
     <Provider store={ store }>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>,
   );
 
